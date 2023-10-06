0316a002c49a5703208392ec197f989ad3da9108
손예섬 <sys8517@deps.kr>
2023-10-06 16:25:55 +0900

    이름 한글로 바꾸기

--- components/PoketCard/PoketCard.tsx
+++ components/PoketCard/PoketCard.tsx
@@ -9,16 +9,24 @@ import {
 
 export default function PoketCard({ name }: { name: string }) {
   const router = useRouter();
+  let ko_name = "";
 
+  //포켓몬의 데이터 불러오기
   const pokeData = useGetPokemonData(name);
+  //포켓몬 종족 정보 불러오기
   const pokeSpecData = useGetPokemonSpeciesData(name);
-
+  if (pokeSpecData.isSuccess) {
+    ko_name = pokeSpecData.data.names.find((name) => {
+      return name.language.name === "ko";
+    }).name;
+    console.log("koname : ", ko_name);
+  }
   //   if (pokeData.isSuccess) {
   //     console.log("pokeData", pokeData.data);
   //   }
-  //   if (pokeSpecData.isSuccess) {
-  //     console.log("pokeSpecData", pokeSpecData.data);
-  //   }
+  if (pokeSpecData.isSuccess) {
+    console.log("pokeSpecData", pokeSpecData.data);
+  }
   //   const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   return (
     <>
@@ -36,7 +44,7 @@ export default function PoketCard({ name }: { name: string }) {
             pokeData.data?.sprites?.front_default
           }
         ></img>
-        <span>{name}</span>
+        <span>{ko_name}</span>
       </div>
     </>
   );
